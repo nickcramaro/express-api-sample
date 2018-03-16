e8bd95ebfef0cc7b441e7fb9bc1add72a48e4e87
Nick Cramaro <ncramaro@konradgroup.com>
2018-03-16 11:55:32 -0400

    Added basic routes

--- server.js
+++ server.js
@@ -4,24 +4,67 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+let movies = [
+    {
+        id: 1,
+        name: 'Shape of Water'
+    }, {
+        id: 2,
+        name: 'Moonlight'
+    }, {
+        id: 3,
+        name: 'Spotlight'
+    }, {
+        id: 4,
+        name: 'Birdman or (The Unexpected Virtue of Ignorance)'
+    }, {
+        id: 5,
+        name: '12 Years a Slave'
+    }, {
+        id: 6,
+        name: 'Argo'
+    }
+];
+
 app.get('/movie', (req, res) => {
-    res.send('Get all movies');
+    res.send(movies);
 });
 
 app.get('/movie/:id', (req, res) => {
-    res.send('Get one movie');
+    let movie = movies.find(m => m.id === Number(req.params.id));
+    res.send(movie);
 });
 
 app.post('/movie', (req, res) => {
-    res.send('Add a movie');
+    let newMovie = {
+        id: movies[movies.length - 1].id + 1,
+        name: req.body.name
+    };
+    movies.push(newMovie);
+    res.send(newMovie);
 });
 
 app.put('/movie', (req, res) => {
-    res.send('Update a movie');
+    let updatedMovie;
+    movies.forEach((m, i) => {
+        if (m.id === req.body.id) {
+            m.name = req.body.name;
+            updatedMovie = m;
+        }
+    });
+    res.send(updatedMovie);
 });
 
 app.delete('/movie/:id', (req, res) => {
-    res.send('Delete a movie');
+    let deletedMovie;
+    for (let i = 0; i < movies.length; i++) {
+        if (movies[i].id === Number(req.params.id)) {
+            deletedMovie = movies[i];
+            movies.splice(i, 1);
+            break;
+        }
+    }
+    res.send(deletedMovie);
 });
 
 app.listen(8080);
